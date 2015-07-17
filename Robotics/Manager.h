3acb910e57b4683fed53719ecdae8e19d96fc529
Or Shainberg <or.shainberg10@gmail.com>
2015-07-16 19:39:12 -0700

    add behaviors

---
+++ Robotics/Manager.h
@@ -0,0 +1,20 @@
+/*
+ * Manager.h
+ *
+ *  Created on: Dec 21, 2014
+ *      Author: user
+ */
+
+#ifndef MANAGER_H_
+#define MANAGER_H_
+#include "Robot.h"
+class Manager {
+	Behavior* _curr;
+	Robot* _robot;
+public:
+	Manager(Robot* robot, Plan* pln);
+	void run();
+	virtual ~Manager();
+};
+
+#endif /* MANAGER_H_ */
